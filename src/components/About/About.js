3ff2920962d3acf4ca8200198edd791695f52030
Sakshi Mittal <sakshimittal3005@gmail.com>
2022-03-25 09:08:38 +0530

    update about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,17 +16,17 @@ export default function About() {
           </h1>
           <h2>Front End Developer</h2>
           <p>
-            I’ve been learning web development for some time and have created
-            interesting projects to showcase my skill set. I'm looking for an
-            entry-level job in the field.
+            {/* I’ve been learning web development for some time and have created
+            interesting projects to showcase my skill set. 
             <br />
-            Please do let me know if you come across any opportunities that fit
-            my skillset. Thanks a lot. My strong skill set is HTML, CSS,
-            JavaScript, Basic ReactJS, and TailwindCSS.
+             My strong skill set is HTML, CSS
+            JavaScript, Basic ReactJS, and TailwindCSS. */}
+            I am a Web Developer based in Gwalior, India with 6 months of experience in developing web applications with Javascript, Reactjs, and jquery,
+            <br/>I am working on Eduvue brand of <a href="https://parakh.online/" style={{"color": "crimson", "text-decoration": "underline", "margin": "0 5px"}} target="_blank">Parakh</a>. It is a complete institute management solution for education institutes, i.e., schools, colleges, universities, coaching centers, tuition centers in India.
           </p>
 
           <a
-            href='/sakshi_resume.pdf'
+            href='/sakshimittal_resume.pdf'
             className='cta resume-btn'
             target='_blank'
           >
