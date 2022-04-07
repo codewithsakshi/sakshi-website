4f18b72598a46993e47f6b6fe2291c2a2b6315f7
Sakshi Mittal <sakshimittal3005@gmail.com>
2022-04-07 15:31:08 +0530

    updated about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,14 +16,14 @@ export default function About() {
           </h1>
           <h2>Front End Developer</h2>
           <p>
-            I am a Web Developer based in Gwalior, India with more than one year of experience in developing web applications with Javascript, Reactjs, and jquery,
+            I am a Web Developer based in Gwalior, India with Two year of experience in developing web applications with Javascript, Reactjs, and jquery,
             <br/>I am working on Eduvue brand of <a href="https://parakh.online/" style={{"color": "crimson", "text-decoration": "underline", "margin": "0 5px"}} target="_blank">Parakh</a>. It is a complete institute management solution for education institutes, i.e., schools, colleges, universities, coaching centers, tuition centers in India.
           </p>
 
           <a
-            href='/sakshi-resume.pdf'
             className='cta resume-btn'
             target='_blank'
+            
           >
             Download Resume
           </a>
