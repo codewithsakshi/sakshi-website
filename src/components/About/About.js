ff3f69b751aa63c459fa86f8bd3fd7bfab101199
Sakshi Mittal <sakshimittal3005@gmail.com>
2022-04-07 13:34:49 +0530

    updated resume on portfolio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,17 +16,12 @@ export default function About() {
           </h1>
           <h2>Front End Developer</h2>
           <p>
-            {/* I’ve been learning web development for some time and have created
-            interesting projects to showcase my skill set. 
-            <br />
-             My strong skill set is HTML, CSS
-            JavaScript, Basic ReactJS, and TailwindCSS. */}
-            I am a Web Developer based in Gwalior, India with 6 months of experience in developing web applications with Javascript, Reactjs, and jquery,
+            I am a Web Developer based in Gwalior, India with more than one year of experience in developing web applications with Javascript, Reactjs, and jquery,
             <br/>I am working on Eduvue brand of <a href="https://parakh.online/" style={{"color": "crimson", "text-decoration": "underline", "margin": "0 5px"}} target="_blank">Parakh</a>. It is a complete institute management solution for education institutes, i.e., schools, colleges, universities, coaching centers, tuition centers in India.
           </p>
 
           <a
-            href='/sakshimittal_resume.pdf'
+            href='/sakshi-resume.pdf'
             className='cta resume-btn'
             target='_blank'
           >
