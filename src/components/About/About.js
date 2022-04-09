f9c92529bd8e0c8109fe03c56008d23a0f1390da
Sakshi Mittal <sakshimittal3005@gmail.com>
2022-04-09 11:15:18 +0530

    updated bio in about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,8 +16,15 @@ export default function About() {
           </h1>
           <h2>Front End Developer</h2>
           <p>
-            I am a Web Developer based in Gwalior, India with Two year of experience in developing web applications with Javascript, Reactjs, and jquery,
+            I am a Web Developer based in Gwalior, India with Two year of experience in developing web applications with HTML, CSS, Javascript, ReactJS, Tailwind CSS, and jquery.
             <br/>I am working on Eduvue brand of <a href="https://parakh.online/" style={{"color": "crimson", "text-decoration": "underline", "margin": "0 5px"}} target="_blank">Parakh</a>. It is a complete institute management solution for education institutes, i.e., schools, colleges, universities, coaching centers, tuition centers in India.
+          <br/>
+          To work as a Frontend developer applying my
+       knowledge in the field of Frontend design and
+        development. I wish to work in a team of
+        motivated individuals who wish to work towards
+           the advancement of the company and its goals.
+         
           </p>
 
           <a
