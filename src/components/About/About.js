3e819eb4d99bb7a8bd497108809044ed0537ac89
codewithsakshi <sakshimittal3005@gmail.com>
2022-08-30 10:32:29 +0530

    format file

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,22 +17,20 @@ export default function About() {
           <h2>Front End Developer</h2>
           <p>
             I am a Web Developer based in Banglore, India with Two year of experience in developing web applications with HTML, CSS, Javascript, ReactJS, Tailwind CSS, and jquery.
-            <br/>I am working in <a href="http://landmarkcares.in/" style={{"color": "crimson", "text-decoration": "underline", "margin": "0 5px"}} target="_blank">Landmark group of technology</a> as a Frontend developer      
-              <br/>
-          To work as a Frontend developer applying my
-       knowledge in the field of Frontend design and
-        development. I wish to work in a team of
-     motivated individuals who wish to work towards
-       the advancement of the company and its goals.
-         
+            <br />I am working in <a href="http://landmarkcares.in/" style={{ "color": "crimson", "text-decoration": "underline", "margin": "0 5px" }} target="_blank">Landmark group of technology</a> as a Frontend developer
+            <br />
+            To work as a Frontend developer applying my
+            knowledge in the field of Frontend design and
+            development. I wish to work in a team of
+            motivated individuals who wish to work towards
+            the advancement of the company and its goals.
           </p>
-
           <a
-          href="/resume.pdf"
-          rel='noreferrer'
-          className='cta resume-btn'
-          target='_blank'
-            
+            href="/resume.pdf"
+            rel='noreferrer'
+            className='cta resume-btn'
+            target='_blank'
+
           >
             Download Resume
           </a>
