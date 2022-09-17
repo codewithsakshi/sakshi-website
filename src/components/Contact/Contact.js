4b78a2c76d8f42c6dd76053828ad6f636f77afe3
codewithsakshi <sakshimittal3005@gmail.com>
2022-09-17 18:49:39 +0530

    update address in contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,18 +11,6 @@ export default function Contact() {
           </h1>
         </div>
         <div className='contact-items-container'>
-          {/* <div className='contact-item'>
-            <div className='icon'>
-              <img
-                src='https://img.icons8.com/bubbles/100/000000/phone.png'
-                alt='contact-icon'
-              />
-            </div>
-            <div className='contact-info'>
-              <h1>Phone</h1>
-              <h2>Not Available</h2>
-            </div>
-          </div> */}
           <div className='contact-item'>
             <div className='icon'>
               <img
@@ -44,7 +32,7 @@ export default function Contact() {
             </div>
             <div className='contact-info'>
               <h1>Address</h1>
-              <h2>GandhiNagar, Gwalior, Madhya pradesh, India</h2>
+              <h2>Bengaluru, Karnataka, India</h2>
             </div>
           </div>
         </div>
